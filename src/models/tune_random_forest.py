9ef6eccbac64ea3daa988fc6d532dc1c73faf5fe
Madhusudan <madhusudansomvanshi55@gmail.com>
2026-05-15 23:22:02 +0530

    Day 5: Added GridSearchCV-based hyperparameter tuning for Random Forest optimization

--- src/models/tune_random_forest.py
+++ src/models/tune_random_forest.py
@@ -6,7 +6,7 @@ def tune_model(X_train, y_train):
     param_grid = {
         'n_estimators' : [100, 200],
         'max_depth' : [None, 20],
-        'main_samples_split' : [2, 5]
+        'min_samples_split' : [2, 5]
     }
 
     rf = RandomForestClassifier(random_state=42)
@@ -16,13 +16,17 @@ def tune_model(X_train, y_train):
         param_grid= param_grid,
         cv= 3,
         scoring= 'f1',
-        n_jobs= -1
+        n_jobs= -1,
+        verbose=2
     )
 
     grid_search.fit(X_train, y_train)
 
-    print("Best parameters: ", grid_search.best_params_)
-    print("Best Score: ", grid_search.best_score_)
+    print("\nBest parameters:")
+    print(grid_search.best_params_)
+
+    print("\nBest F1 Score: ")
+    print(grid_search.best_score_)
 
 
     return grid_search.best_estimator_
--- src/pipeline/train.py
+++ src/pipeline/train.py
@@ -1,15 +1,16 @@
 from sklearn.model_selection import train_test_split
-from src.models.random_forest import get_model
+from src.models.tune_random_forest import tune_model
 
 def train_model(X, y):
     X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.2, 
-    random_state=42,
-    stratify=y
+    X,
+    y,
+    test_size=0.2,
+    random_state=42
     )
 
-    model = get_model()
-    model.fit(X_train, y_train)
-    print("Model Trained Successfully")
-    
+    model = tune_model(X_train, y_train)
+
+    print("Best Tuned Model Trained Successfully")
+
     return model, X_test, y_test
